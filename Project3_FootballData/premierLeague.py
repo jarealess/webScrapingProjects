9a2ac12d134cf62426f3a908ab7732cf18d3dbf5
jarealess <jarealess@unal.edu.co>
2021-12-01 17:14:54 -0500

    DataFrame con diferentes stats

--- Project3_FootballData/premierLeague.py
+++ Project3_FootballData/premierLeague.py
@@ -23,14 +23,17 @@ from selenium.webdriver.support import expected_conditions as EC
 
 #-------------------------------------  config browser ------------------------------------- 
 
-playerUrl = 'https://www.premierleague.com/stats/top/players/goals'
+webUrl = 'https://www.premierleague.com/stats/top/players/goals'
 
 
-option = webdriver.ChromeOptions()
-option.add_experimental_option("excludeSwitches", ['enable-automation'])
-browser = webdriver.Chrome(executable_path='C:\webdriver\chromedriver.exe', options=option) ## path al chrome driver
-browser.get(playerUrl)
-time.sleep(5)
+def funcBrowser(webUrl):
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("excludeSwitches", ['enable-automation'])
+    browser = webdriver.Chrome(executable_path='C:\webdriver\chromedriver.exe', options=option) ## path al chrome driver
+    browser.get(webUrl)
+    time.sleep(5)
+
+    return browser
 
 
 #------------------------------------- 'accepting' cookies popup  ------------------------------------- 
@@ -68,22 +71,29 @@ def currentSeason(browser):
 
 #------------------------------ lists to fill in with needed information ------------------------------------- 
 
-def funcGettingStats():
+def funcGettingStats(browser, statType):
+
+    funcHandleCookies(browser)  # accept cookies
+    currentSeason(browser)      #select current season
+
     playerNames = []
     playerStats = []
-
+    
     while True:
 
         try:
             WebDriverWait(browser, 3).until(
                         EC.presence_of_element_located((By.XPATH,'//*[@id="mainContent"]/div[2]/div/div[2]/div[1]/div[3]/div[2]')))
 
+            time.sleep(2)
             arrow = browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div/div[2]/div[1]/div[3]/div[2]')
             browser.execute_script("arguments[0].click();",arrow)
-        except StopIteration:
-         print('Not located')
+        except:
+             print('Not located')
         
-        bs = BeautifulSoup(browser.page_source, 'html.parser')
+        time.sleep(1)
+
+        bs = BeautifulSoup(browser.page_source, 'lxml')
         Names = bs.find_all('a', {'class':'playerName'})
         stats_ = bs.find_all('td', {'class':'mainStat text-centre'})
         
@@ -91,13 +101,15 @@ def funcGettingStats():
         if Names[-1].find('strong').text in playerNames:
             break
 
-        for i in range(len(Names)):
-            playerNames.append(Names[i].find('strong').text)
-            playerStats.append(stats_[i].text) 
+        for j in range(len(Names)):
+            playerNames.append(Names[j].find('strong').text)
+            playerStats.append(stats_[j].text) 
+        
+        time.sleep(0.5)
 
 
 
-    df1 = pd.DataFrame({"Names": playerNames, 'MainStat':playerStats}, columns=['Names', 'MainStat'])
+    df1 = pd.DataFrame({"Names": playerNames, statType:playerStats}, columns=['Names', statType])
 
     browser.close()
     
@@ -108,15 +120,37 @@ def funcGettingStats():
 
 ## ----------------------------------Execution -------------------------------------------------------------
 
-funcHandleCookies(browser)
-currentSeason(browser)
 
-dfGoals = funcGettingStats()
+## Appearances
+browser = funcBrowser('https://www.premierleague.com/stats/top/players/appearances')
+dfAppearance = funcGettingStats(browser, 'Appearances')
 
-print(dfGoals)
+## goals
+browser = funcBrowser('https://www.premierleague.com/stats/top/players/goals')
+dfGoals = funcGettingStats(browser, 'Goals')
 
 
+## assists
+browser = funcBrowser('https://www.premierleague.com/stats/top/players/goal_assist')
+dfAssists = funcGettingStats(browser, 'Assists')
 
+
+## Shots
+browser = funcBrowser('https://www.premierleague.com/stats/top/players/total_scoring_att')
+dfShots = funcGettingStats(browser, 'Shots')
+
+
+#yellow cards
+browser = funcBrowser('https://www.premierleague.com/stats/top/players/yellow_card')
+dfYellow = funcGettingStats(browser, 'YellowCards')
+
+
+df1 = pd.merge(dfAppearance, dfGoals, on='Names', how='left')
+df2 = pd.merge(df1, dfAssists, on='Names', how='left')
+df3 = pd.merge(df2, dfShots, on='Names', how='left')
+df4 = pd.merge(df3, dfYellow, on='Names', how='left')
+
+print(df4)
 
 
 
