b468df5f2bfcc807fc9614a23a5d2de0cb65b6a0
jarealess <jarealess@unal.edu.co>
2021-12-05 09:17:52 -0500

    Function to get flights

--- Project2_TicketPriceAnalysis/TicketPrice.py
+++ Project2_TicketPriceAnalysis/TicketPrice.py
@@ -7,103 +7,108 @@
 # send an email containing the information that you extracted from the website to yourself.
 
 
-## libraries
+##--------- libraries
+import time
+import pandas as pd
 from bs4 import BeautifulSoup
 import selenium
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import numpy as np
-import pandas as pd
 
 
-## config browser
+
+##--------- config browser
 option = webdriver.ChromeOptions()
 option.add_experimental_option("excludeSwitches", ['enable-automation'])
 browser = webdriver.Chrome(executable_path='C:\webdriver\chromedriver.exe', options=option) ## path al chrome driver
 
 
-## inputs
-## se ingresa el código IATA del origen y destino y una fecha. 
-source='MDE'
-destination='ADZ'
-date='2022-01-23'
-url=("https://www.kayak.com.co/flights/{0}-{1}/{2}?sort=bestflight_a".format(source,destination,date))
-browser.get(url)
+#---------------- function to get flights from KAYAK
+
+def findFlights(source,destination,date):
 
 
-## loading more flights
-## Esta sección sirve para hacer clic en el botón (ver más resultados)
-for i in range(5):
-    try:
-        WebDriverWait(browser, 5).until(
-                        EC.presence_of_element_located((By.CLASS_NAME, 'resultsPaginator')))
-
-        time.sleep(7)
-        element = browser.find_element_by_class_name('moreButton')
-        browser.execute_script("arguments[0].click();",element)
-        time.sleep(3)
-    except:
-        print(f"Element not located in iteration {i}")
+    url=("https://www.kayak.com.co/flights/{0}-{1}/{2}?sort=bestflight_a".format(source,destination,date))
+    browser.get(url)
 
 
-## getting data
-bs = BeautifulSoup(browser.page_source, 'html.parser')
-horario = bs.find_all('div', {'class':'section times'})
-prices = bs.find_all('div', {'class':'multibook-dropdown'})
-airline = bs.find_all('span', {'class':'codeshares-airline-names'})
-flightType = bs.find_all('div', {'class':'section stops'})
-fLinks = bs.find_all('a', {'class':'booking-link'})
+    #---------------------- loading more flights
+    #---------------------- Esta sección sirve para hacer clic en el botón (ver más resultados)
+    for i in range(5):
+        try:
+            WebDriverWait(browser, 5).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, 'resultsPaginator')))
+
+            time.sleep(7)
+            element = browser.find_element_by_class_name('moreButton')
+            browser.execute_script("arguments[0].click();",element)
+            time.sleep(3)
+        except:
+            print(f"Element not located in iteration {i}")
+
+
+    #------------- getting data
+    bs = BeautifulSoup(browser.page_source, 'html.parser')
+    details = bs.find_all('div', {'class':'Base-Results-HorizonResult'})
+
+    listHorario = []
+    listPrices = []
+    listAirlines = []
+    listStops = []
+    listLinks = []
+
+    for i in range(len(details)):
+
+        ##--- schedule
+        horario = details[i].find('div', {'class':'section times'})
+        h1 = horario.find('span', {'class':'depart-time base-time'})
+        h2 = horario.find('span', {'class':'arrival-time base-time'})
+
+        if h1 is not None:
+            listHorario.append('{0}-{1}'.format(h1.text,h2.text).encode('utf-8').decode())
+
+        #--- prices 
+        prices = details[i].find('div', {'class':'multibook-dropdown'})
+        p1 = prices.find('span', {'class':'price-text'})
+        listPrices.append(float(p1.text[2:].replace('.','')))
+
+        #--- airlines
+        airline = details[i].find('span', {'class':'codeshares-airline-names'})
+        listAirlines.append(airline.text) 
+
+        #--- stops
+        flightType = details[i].find('div', {'class':'section stops'})
+        f1 = flightType.find('span', {'class':'stops-text'})
+        f2 = flightType.find('span', {'class':'js-layover'})
+
+        if f2 is not None:
+            listStops.append(f2.text.strip())
+        else:
+            listStops.append(f1.text.replace('\n',''))
+
+        ##--- links
+        fLinks = details[i].find('a', {'class':'booking-link'})
+        listLinks.append('https://www.kayak.com.co{0}'.format(fLinks.attrs['href']))
+
+
+    browser.close()
+    
+    ## dataframe
+    dictFlights = {'Aerolinea':listAirlines, 'Horario':listHorario, 'Precio':listPrices, 'Escalas':listStops, 'Links':listLinks}
+    dfFlights = pd.DataFrame(dictFlights, columns=['Aerolinea','Horario','Precio','Escalas','Links'])
+    dfFlights = dfFlights.sort_values(by='Precio', ascending=True).drop_duplicates()
+
+    return dfFlights
 
 
 
-## filling lists
-minVal = min(len(horario),len(prices),len(airline),len(flightType),len(fLinks))
 
-listHorario = []
-listPrices = []
-listAirlines = []
-listStops = []
-listLinks = []
-
-for i in range(minVal):
-
-    ## schedule
-    h1 = horario[i].find('span', {'class':'depart-time base-time'})
-    h2 = horario[i].find('span', {'class':'arrival-time base-time'})
-
-    if h1 is not None:
-        listHorario.append('{0}-{1}'.format(h1.text,h2.text).encode('utf-8').decode())
-
-    #prices 
-    p1 = prices[i].find('span', {'class':'price-text'})
-    listPrices.append(float(p1.text[2:].replace('.','')))
-
-    # airlines
-    listAirlines.append(airline[i].text) 
-
-    # stops
-    f1 = flightType[i].find('span', {'class':'stops-text'})
-    f2 = flightType[i].find('span', {'class':'js-layover'})
-
-    if f2 is not None:
-      listStops.append('{0}-{1}'.format(f1.text,f2.text).encode('utf-8').decode())
-    else:
-        listStops.append(f1.text)
-
-    ##links
-    listLinks.append('https://www.kayak.com.co{0}'.format(fLinks[i].attrs['href']))
-
-
-## dataframe
-dictFlights = {'Aerolinea':listAirlines, 'Horario':listHorario, 'Precio':listPrices, 'Paradas':listStops, 'Links':listLinks}
-dfFlights = pd.DataFrame(dictFlights, columns=['Aerolinea','Horario','Precio','Paradas','Links'])
-dfFlights.drop_duplicates()
+dfFlights = findFlights('MDE','ADZ','2022-01-23')
 
 ## 15 vuelos más baratos
-print(dfFlights.sort_values(by='Precio', ascending=True).iloc[:15])
+print(dfFlights.iloc[:15])
 
 # csv
-dfFlights.to_csv('cheapestFlights.csv', encoding='utf-8')
+dfFlights.to_csv('cheapestFlights.csv', encoding='utf-8', index=False)
