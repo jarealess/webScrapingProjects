b81045d74904e9107a31a62fcf4376fb92d5e5c0
jarealess <jarealess@unal.edu.co>
2021-12-13 17:32:01 -0500

    Funciones para extraer información

--- Project5_Cryptocurrencies/cryptoCurrency.py
+++ Project5_Cryptocurrencies/cryptoCurrency.py
@@ -7,78 +7,131 @@
 # last seven days’ trend, etc. One can find these details on CoinMarketCap.
 
 
-
-import os
-import time
-from bs4 import BeautifulSoup
+#Librerías
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import requests
+import time
 
 
 
-# driver.get("https://www.coingecko.com/en")
-time.sleep(5)
-
-pageUrl = requests.get('https://www.coingecko.com/en?page=1')
-
-#--------------- obtenemos la información de cada criptomoneda
-
-bs = BeautifulSoup(pageUrl.text, 'lxml')
-trS = bs.find('tbody').find_all('tr')
-LastPage = bs.find_all('li', class_='page-item')[-2].text
+##--------- config browser
+option = webdriver.ChromeOptions()
+option.add_argument('--log-level=3')  ## Para suprimir mensajes/advertencias en consola
+option.add_experimental_option("excludeSwitches", ['enable-automation'])
+browser = webdriver.Chrome(executable_path='C:\webdriver\chromedriver.exe', options=option) 
 
 
-coinList = []
-sNameList = []
-pricesList = []
-oneHourRate = []
-oneDayRate = []
-sevenDaysRate = []
-oneDayVolume = []
-MktCap = []
 
+## -------------- función para obtener información de Criptomonedas
+def fnGetCryptoCoins():
 
-for i in range(1,10):#range(1,int(LastPage)):
-    
-    url = ('https://www.coingecko.com/en?page='+str(i))
-    pageUrl = requests.get(url)
-    trS = BeautifulSoup(pageUrl.text, 'lxml').find('tbody').find_all('tr')
+  
+    #----- solo se tomarrá la información de la primera página
+    browser.get('https://www.coingecko.com/en?page=1')
+    time.sleep(2)
+    trS = BeautifulSoup(browser.page_source, 'lxml').find('tbody').find_all('tr')
+
+   
+    browser.close()
+    # ------- Lists
+
+    coinList, sNameList, pricesList, oneHourRate = [], [], [], []
+    oneDayRate, sevenDaysRate, oneDayVolume, MktCap, Last7Days = [], [], [], [],[] 
+
 
 
     for tr in trS:
         coinList.append(tr.find('a', class_='tw-hidden lg:tw-flex font-bold tw-items-center tw-justify-between').text.replace('\n',''))
         sNameList.append(tr.find('span', class_='tw-hidden d-lg-inline font-normal text-3xs ml-2').text.replace('\n','') ) 
-        pricesList.append(tr.find('td', class_='td-price').find('span').text.replace('\n','').replace('$','').replace(',',''))  
-        MktCap.append(tr.find('td', class_='td-market_cap').find('span').text.replace('\n','').replace('$','').replace(',','')) 
+        pricesList.append(tr.find('td', class_='td-price').find('span').text.replace('\n',''))  
+        MktCap.append(tr.find('td', class_='td-market_cap').find('span').text.replace('\n','')) 
+        Last7Days.append(tr.find('td', class_='p-0 pl-2 text-center').find('a').attrs['href'])  #links
 
 
+        ##---- esta información viene con algunos None
         oneH = tr.find('td', class_='td-change1h').find('span')
         oneDay = tr.find('td', class_='td-change24h').find('span')
         sevenDay = tr.find('td', class_='td-change7d').find('span')
         oneDayV = tr.find('td', class_='td-liquidity_score').find('span')
 
+
+        ## ------- donde encuentre un None, no inserte
         if None not in [oneH,oneDay,sevenDay,oneDayV]:
-            oneHourRate.append(oneH.text.replace('\n','').replace('%',''))
-            oneDayRate.append(oneDay.text.replace('\n','').replace('%',''))
-            sevenDaysRate.append(sevenDay.text.replace('\n','').replace('%',''))
-            oneDayVolume.append(oneDayV.text.replace('\n','').replace('$','').replace(',',''))
+            oneHourRate.append(oneH.text.replace('\n',''))
+            oneDayRate.append(oneDay.text.replace('\n',''))
+            sevenDaysRate.append(sevenDay.text.replace('\n',''))
+            oneDayVolume.append(oneDayV.text.replace('\n',''))
         else: 
             oneHourRate.append('')
             oneDayRate.append('')
             sevenDaysRate.append('')
             oneDayVolume.append('')
     
-    print(i)
+    ##---- se almacenan las listas un diccionario 
+
+    dictCrypto = {'Coin':coinList, 'Short Name':sNameList, 'Price':pricesList, '1h':oneHourRate, '24h':oneDayRate,
+                        '7d':sevenDaysRate, '24h Volume':oneDayVolume, 'Marketing Cap':MktCap, 'Last 7d Trend Links':Last7Days}
+    
+    return dictCrypto
+
+
+
+
+# --- función para obtener datos de una moneda deseada
+
+def fnPrintCoinInfo(NameType, CoinName):
+
+    dictCrypto = fnGetCryptoCoins()
+
+    # ----------- se define el nombre a usar
+    if NameType==1:
+        _CoinNameType = 'Coin'
+    else:
+        _CoinNameType = 'Short Name'
+
+    # -------- indice
+    index_ = dictCrypto[_CoinNameType].index(CoinName)
+
+    # --- se imprime información
+    for key, value in dictCrypto.items():
+        print(f'{key}: {value[index_]}')
+
     
 
 
-print(MktCap)
-print(len(MktCap))
+# ---- evolutivo precio últimos 7 días
+
+def fnPriceLastWeek(CoinName):
+
+    # ---- url
+    
+    url = (f'https://www.coingecko.com/en/coins/{CoinName.lower()}/usd?chart=7_days#panel')
+    browser.get(url)
+    time.sleep(2)
+
+    # ---- prices data
+    trS = BeautifulSoup(browser.page_source, 'html.parser').find('tbody').find_all('tr')
+
+    browser.close()
+    dict7dChange = {}
+
+    for tr in trS:
+
+        # --- Date
+        Date = tr.find('th').text.replace('\n','')
+
+        # --- price in USD Dollars
+        UsdPrice = tr.find_all('td')[1].text.replace('\n','').replace('$','').replace(',','')
+        
+        dict7dChange[Date] = UsdPrice
+        print(f'{Date}: {float(UsdPrice)}')
+    
 
 
 
 
-
-
+#fnPrintCoinInfo(1, 'Bitcoin')
+fnPriceLastWeek('Bitcoin')
 
 
--- Project5_Cryptocurrencies/test.py
+++ Project5_Cryptocurrencies/test.py
@@ -1,3 +1,37 @@
-for i in range(1,101):
-    xpath_=('//*[@id="__next"]/div/div[1]/div[2]/div/div/div[5]/table/tbody/tr['+str(i)+']/td[3]/div/a/div/div/p')
-    print(xpath_)
+
+
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import requests
+import time
+from selenium import webdriver
+
+
+
+
+option = webdriver.ChromeOptions()
+option.add_argument('--log-level=3')  ## Para suprimir mensajes/advertencias en consola
+option.add_experimental_option("excludeSwitches", ['enable-automation'])
+browser = webdriver.Chrome(executable_path='C:\webdriver\chromedriver.exe', options=option) 
+
+
+url = ('https://www.coingecko.com/en/coins/bitcoin/usd?chart=7_days#panel')
+browser.get(url)
+
+trS = BeautifulSoup(browser.page_source, 'html.parser').find('tbody').find_all('tr')
+
+dict7dChange = {}
+for tr in trS:
+
+    # --- Date
+    Date = tr.find('th').text.replace('\n','')
+
+    # --- price in USD Dollars
+    UsdPrice = tr.find_all('td')[1].text.replace('\n','').replace('$','').replace(',','')
+    
+    dict7dChange[Date] = UsdPrice
+    print(f'{Date}: {float(UsdPrice)}')
+
+
+
+
