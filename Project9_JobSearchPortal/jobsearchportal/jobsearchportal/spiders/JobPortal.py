e5a509c06155760e2996bfc63d7f62fca9fd0dce
jarealess <jarealess@unal.edu.co>
2022-01-27 16:05:11 -0500

    Jobs Filtered by Parameters

--- Project9_JobSearchPortal/jobsearchportal/jobsearchportal/spiders/JobPortal.py
+++ Project9_JobSearchPortal/jobsearchportal/jobsearchportal/spiders/JobPortal.py
@@ -1,12 +1,18 @@
 import scrapy
-import re
+
 
 class JobportalSpider(scrapy.Spider):
     name = 'JobPortal'
-    allowed_domains = ['www.elempleo.com/co/ofertas-empleo/?trabajo=']
+    #allowed_domains = ['www.elempleo.com/co/ofertas-empleo/?trabajo=']
 
-    jobSearched = 'sql'
-    start_urls = ['http://www.elempleo.com/co/ofertas-empleo/?trabajo='+jobSearched]
+
+    def start_requests(self):
+       # jobSearched = self.jobarg
+        urls = ['http://www.elempleo.com/co/ofertas-empleo/?trabajo='+self.jobarg]
+        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
 
     def parse(self, response):
         
@@ -14,12 +20,15 @@ class JobportalSpider(scrapy.Spider):
 
         i = 0
         for job in jobsEE:
-            
-            if self.jobSearched in job.xpath('./li[1]/h2/a/text()').get().replace('\r\n','').strip():
+
+            jobTitle = job.xpath('./li[1]/h2/a/text()').get().replace('\r\n','').strip()
+            isMatch = [True for x in getattr(self, 'jobarg2').split(',') if x in jobTitle]
+            if True in isMatch:
                 i+=1 
-                yield {f'Post{i}': job.xpath('./li[1]/h2/a/text()').get().replace('\r\n','').strip(),
+                yield {f'Post{i}': jobTitle,
                         'Company': job.xpath('./li[2]/h3/span[2]/span/text()').get().replace('\r\n','').strip(),
                         'Salary':job.xpath('./li[3]/span[1]/text()').get().replace('\r\n','').strip(),
                         'City':job.xpath('./li[3]/span[2]/span/span[1]').get()[120:170].replace('\r\n','').strip(),
                         'Date': job.xpath('./li[3]/span[3]').get()[120:180].replace('\r\n','').strip()
                     }
+
